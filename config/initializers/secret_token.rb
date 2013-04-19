546eafce750edaf078163bed099e508142bcb4fb
Tad Thorley <phaedryx@gmail.com>
2013-04-18 21:25:12 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAngularjsExample::Application.config.secret_token = '1ef6dba9b5a891ef08e3e9f0345459dcbf0216464790fcac8fb62da438517127b60bc8f52dad0654dbfd3f6362ae752f91ec7bebc042ed8e18be308d20a1245f'
